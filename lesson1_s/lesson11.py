fee555d16b6e7a11981c7c668705a11aca928da4
sonicx93 <alexandedomnin1993@gmail.com>
2020-10-11 18:36:40 +0300

    Add new homeworks 11/10

--- lesson1_s/lesson11.py
+++ lesson1_s/lesson11.py
@@ -7,28 +7,21 @@ try:
     browser.get(link)
 
     # Ваш код, который заполняет обязательные поля
-    input1= browser.find_element_by_css_selector(".first_class>input")
-    input1.send_keys("Alex")
-    input2 = browser.find_element_by_xpath("//*[normalize-space(text())='Last name*']/following-sibling::input")
-    input2.send_keys("Domnin")
-    input3 = browser.find_element_by_css_selector(".third_class>input")
-    input3.send_keys("gog@ya.ru")
-
+    input1 = browser.find_element_by_xpath("//*[normalize-space(text())='First name*']/following-sibling::input").send_keys("Alex")
+    input2 = browser.find_element_by_xpath("//*[normalize-space(text())='Last name*']/following-sibling::input").send_keys("Domnin")
+    input3= browser.find_element_by_xpath("//*[normalize-space(text())='Email*']/following-sibling::input").send_keys("gog@ya.ru")
     # Отправляем заполненную форму
-    button = browser.find_element_by_xpath("//*[@type='submit']")
-    button.click()
-
+    button = browser.find_element_by_xpath("//*[@type='submit']").click()
     # Проверяем, что смогли зарегистрироваться
     # ждем загрузки страницы
     time.sleep(1)
-
     # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text_elt = browser.find_element_by_tag_name("h1").text
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
+    #welcome_text = welcome_text_elt.text
 
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert "Congratulations! You have successfully registered!" == welcome_text
+    assert "Congratulations! You have successfully registered!" == welcome_text_elt
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
---
+++ lesson3/32s13.py
@@ -0,0 +1,36 @@
+import unittest
+from selenium import webdriver
+import math
+
+
+class Test11(unittest.TestCase):
+    def test_1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element_by_xpath(
+            "//*[normalize-space(text())='First name*']/following-sibling::input").send_keys("Alex")
+        input2 = browser.find_element_by_xpath(
+            "//*[normalize-space(text())='Last name*']/following-sibling::input").send_keys("Domnin")
+        input3 = browser.find_element_by_xpath(
+            "//*[normalize-space(text())='Email*']/following-sibling::input").send_keys("gog@ya.ru")
+        button = browser.find_element_by_xpath("//*[@type='submit']").click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element_by_tag_name("h1").text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text_elt, 'Text ne sovpadaet')
+    def test_2(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        input1 = browser.find_element_by_xpath(
+            "//*[normalize-space(text())='First name*']/following-sibling::input").send_keys("Alex")
+        input2 = browser.find_element_by_xpath(
+            "//*[normalize-space(text())='Last name*']/following-sibling::input").send_keys("Domnin")
+        input3 = browser.find_element_by_xpath(
+            "//*[normalize-space(text())='Email*']/following-sibling::input").send_keys("gog@ya.ru")
+        button = browser.find_element_by_xpath("//*[@type='submit']").click()
+        time.sleep(1)
+        welcome_text_elt = browser.find_element_by_tag_name("h1").text
+        self.assertEqual("Congratulations! You have successfully registered!", welcome_text_elt, 'Text ne sovpadaet')
+if __name__ == "__main__":
+    unittest.main()
