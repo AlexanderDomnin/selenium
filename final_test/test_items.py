0c697d658254c2c85ff61e7f701d026c4d308b57
sonicx93 <alexandedomnin1993@gmail.com>
2021-02-28 19:03:53 +0300

    задание части 3.6 запуск автотестов для разных языков интерфейса

--- final_test/test_items.py
+++ final_test/test_items.py
@@ -1,7 +1,7 @@
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coder66s-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button_pass(browser):
+def test_find_button_basket(browser):
     browser.get(link)
     markettext = len(browser.find_elements_by_xpath("//*[@id='add_to_basket_form']/button[@type='submit']"))
     assert markettext > 0, \
